62e8f1268d9b5512d9cdefb90217b90acbd0a26e
Usuario <Usuario@DESKTOP-P9PU196>
2019-06-02 23:34:37 +0200

    FINAL

--- tetris/App.java
+++ tetris/App.java
@@ -1,9 +1,6 @@
 package tetris;
 
 import excepciones.ExcepcionSql;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import static tetris.Menu.crearMenu;
 
 public class App {
@@ -11,13 +8,11 @@ public class App {
     static int INDEX;
 
     public App() {
-
         try {
             crearMenu();
         } catch (ExcepcionSql ex) {
             ex.getMessage();
         }
-
     }
 
 }
