cacafd88b9b741ad3615c35e1f0740c0ae91b429
Usuario <Usuario@DESKTOP-P9PU196>
2019-06-03 11:48:15 +0200

    ORDENADOR EXPOSICION TETRIS 99

--- sonido/SonidoBorrarLinea.java
+++ sonido/SonidoBorrarLinea.java
@@ -10,8 +10,8 @@ public class SonidoBorrarLinea extends Thread {
         try {
             Clip sonido = AudioSystem.getClip();
 //            File a = new File("C:\\Users\\Usuario\\Desktop\\Balloon.wav.wav");//pc casa
-            File a = new File("/home/s7orm/Escritorio/Balloon.wav.wav");//pc portatil
-//            File a = new File("/home/local/DANIELCASTELAO/igonzaleziglesias/Escritorio/Balloon.wav");//pc clase
+//            File a = new File("/home/s7orm/Escritorio/Balloon.wav.wav");//pc portatil
+            File a = new File("/home/local/DANIELCASTELAO/igonzaleziglesias/NetBeansProjects/Tetris/ProyectoProgTetris/wavs/Balloon.wav");//pc clase
             sonido.open(AudioSystem.getAudioInputStream(a));
             sonido.start();
             Thread.sleep(200); // 200 milisegundos (0.2segundos)
